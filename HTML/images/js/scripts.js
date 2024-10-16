fde119fe56e5732a7a8a089b76cfcb53a5cb672a
Odhiambo254 <odhiamboandrew76@gmail.com>
2024-10-16 11:08:23 +0300

    first html files

---
+++ HTML/images/js/scripts.js
@@ -0,0 +1,6 @@
+const colorButton=document.getElementById("colorButton");
+
+colorButton.addEventListener("click", function(){
+
+    colorButton.style.backgroundColor=colorButton.style.backgroundColor="white"
+})
